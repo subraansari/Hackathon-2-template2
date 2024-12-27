174d786ba8ee513c38868c86aa22b2fc2e74f601
Subra Ansari <164074216+subraansari@users.noreply.github.com>
2024-12-27 19:15:49 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import Hero from "./components/Hero";
+import Brand from "./components/brand";
+import Ceramics from "./components/ceramics";
+import Product from "./components/product";
+import Benefit from "./components/benefit";
+import Touch from "./components/touch";
+import ProductListing from "./productlisting/page";
+
+export default function Home() {
+  return (
+   <>
+   <Hero />
+   <Brand />
+   <Ceramics />
+   <Product />
+   <Benefit />
+   <Touch />
+<ProductListing/>
+   
+
+   </>
+  );
+}
